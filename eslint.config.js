8e20f190e45559ee572ba1245af412acb08cd92e
aria-sharifnia <aria.sharifnia@gmail.com>
2025-08-02 15:48:50 -0600

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import react from 'eslint-plugin-react/configs/recommended.js'
+import prettier from 'eslint-config-prettier'
+import globals from 'globals'
+import pluginPrettier from 'eslint-plugin-prettier'
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  react,
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      prettier: pluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  prettier,
+]
