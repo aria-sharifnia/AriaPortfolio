8ccf88cd5b42ea921afeb7f6c79f6299b97eb0f7
aria-sharifnia <aria.sharifnia@gmail.com>
2025-08-27 13:48:29 -0600

    adding speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
 import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import App from './App'
 import './styles/globals.css'
 
@@ -38,6 +39,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   >
     <QueryClientProvider client={queryClient}>
       <App />
+      <SpeedInsights />
       {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   </PersistQueryClientProvider>
