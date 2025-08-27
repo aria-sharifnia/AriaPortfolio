5b2ffe0a432ca227eb5e57208bb5bd7cbd86c15c
aria-sharifnia <aria.sharifnia@gmail.com>
2025-08-27 16:46:01 -0600

    removing query react tools

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
 import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister'
 import { SpeedInsights } from '@vercel/speed-insights/react'
@@ -43,7 +42,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <App />
       <SpeedInsights />
-      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   </PersistQueryClientProvider>
 )
